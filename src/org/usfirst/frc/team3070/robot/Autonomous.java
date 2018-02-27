da7c60731b5ea89530cc31e677b498867b1235df
beechnutkjr <xseggs@gmail.com>
2018-02-26 16:18:31 -0800

    github commit and removal of exchange code

--- src/org/usfirst/frc/team3070/robot/Autonomous.java
+++ src/org/usfirst/frc/team3070/robot/Autonomous.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team3070.robot;
 
 import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.DigitalInput;
 
@@ -53,63 +52,8 @@ public class Autonomous implements Pronstants {
 		double initGyro = prontoGyro.getRawHeading();
 	
 		// the list of steps that the robot needs to do in auto
-		switch (autoSteps) {
-		// this step makes the robot go straight
-		case FIRST_STRAIGHT:
-			// robot drives the distance defined by firstDist
-			if (drive.talLM.getSelectedSensorPosition(0) <= 2000) {
-				drive.simpleDrive(AUTO_SPEED, AUTO_SPEED);
-			} else {
-				drive.stop();
-				// this advances the step
-				nextStep(AutoSteps.FIRST_TURN);
-			}
-			break;
-		//Turns within 3 degrees of the exchange
-		case FIRST_TURN:
-			//checks to adjust for angle
-			if(Math.abs(initGyro - prontoGyro.getRawHeading()) >= 3) {
-				//drive.(Math.abs(initGyro - prontoGyro.getRawHeading()));
-		}else {
-			//stops once it has reached an acceptable angle
-			drive.stop();
-		}
-			break;
-		// this step is the straight away for getting the robot in range to the exchange
-		case SECOND_STRAIGHT:
-			//goes half of a rotation
-			if (drive.talLM.getSelectedSensorPosition(0) <= 2000) {
-				drive.simpleDrive(AUTO_SPEED, AUTO_SPEED);
-			} else {
-				//stops when it reaches the desired location
-				drive.stop();
-				// this advances the step
-				nextStep(AutoSteps.FIRST_TURN);
-			}
-			break;
-			//shoots out the cube
-		case DISPENSE:
-			if(false) { // TODO Fix this
-				grabber.ungrab();
-			}else {
-				//when the cube is out, the grabber stops
-				grabber.stop();
-				nextStep(AutoSteps.THIRD_STRAIGHT);
-			}
-			break;
-			//beeline across the line
-		case THIRD_STRAIGHT:
-			if (drive.talLM.getSelectedSensorPosition(0) <= 30000) {
-				drive.simpleDrive(AUTO_SPEED, AUTO_SPEED);
-			} else {
-				//once the robot crosses the line, roughly, stops
-				drive.stop();
-				// this advances the step
-				nextStep(AutoSteps.FIRST_TURN);
-			}
-			//job is done
-			break;
-		case STOP:
+		switch(autoStep) {
+		
 		}
 	}
 }		
