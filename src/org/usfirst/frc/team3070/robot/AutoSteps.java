50cbc422d241b3233fbda322d5b06cc0f158d83b
Wisestgut <rs0977624@k12.shorelineschools.org>
2018-03-08 15:55:48 -0800

    this has code for center starting auto
    starting in the center, the robto will turn and place the cube in the switch

--- src/org/usfirst/frc/team3070/robot/AutoSteps.java
+++ src/org/usfirst/frc/team3070/robot/AutoSteps.java
@@ -1,5 +1,5 @@
 package org.usfirst.frc.team3070.robot;
 
 public enum AutoSteps {
-	FIRST_STRAIGHT, LOADING, DONE
+	FIRST_STRAIGHT, FIRST_TURN, SECOND_STRAIGHT, LOADING, DONE
 }
--- src/org/usfirst/frc/team3070/robot/Autonomous.java
+++ src/org/usfirst/frc/team3070/robot/Autonomous.java
@@ -3,9 +3,12 @@ package org.usfirst.frc.team3070.robot;
 public class Autonomous implements Pronstants {
 	Drive drive;
 	Grabber grabber;
+	private static BNO055 imu;
 	AutoSteps autoStep = AutoSteps.FIRST_STRAIGHT;
 	String gameData, switchPos, startPos;
 	boolean done = false;
+	double initHeading = imu.getHeading();
+	double currHeading = 0;
 
 	/**
 	 * Constructor
@@ -15,11 +18,18 @@ public class Autonomous implements Pronstants {
 	 * @param grabber
 	 *            Grabber instance
 	 */
-	public Autonomous(Drive drive, Grabber grabber) {
+	public Autonomous(Drive drive, Grabber grabber, ProntoGyro prontoGyro) {
 		this.drive = drive;
 		this.grabber = grabber;
+		imu = BNO055.getInstance(BNO055.opmode_t.OPERATION_MODE_IMUPLUS, BNO055.vector_type_t.VECTOR_EULER);
+
 	}
 
+	public double rawHeading() {
+		return imu.getHeading() - initHeading;
+	}
+	
+	
 	/**
 	 * Run when want to go to next step. Stops motors and sets the enum var to the
 	 * argument
@@ -33,7 +43,7 @@ public class Autonomous implements Pronstants {
 		// Tells the robot to go to the next step
 		autoStep = next;
 		System.out.println("Next step: " + autoStep);
-
+		currHeading = imu.getHeading();
 		drive.resetEncDist();
 		// Stop the robot
 		drive.stop();
@@ -46,23 +56,55 @@ public class Autonomous implements Pronstants {
 		// the list of steps that the robot needs to do in auto
 		switch (autoStep) {
 		case FIRST_STRAIGHT:
-			if(drive.driveDistance(AUTO_SPEED, SWITCH_TICKS)) {
-				// TODO Figure out why gameData.length() isn't working
-				if(/*gameData.length() > 0 &&*/ /*Check if there is any game data first*/ startPos.equals(switchPos)) {
-					nextStep(AutoSteps.LOADING);
-				} else {
-					nextStep(AutoSteps.DONE);
+			// TODO Figure out why gameData.length() isn't working
+			if (/* gameData.length() > 0 && */ /* Check if there is any game data first */ startPos.equals("Center")) {
+				if (drive.driveDistance(AUTO_SPEED, ROTATE * 4)) {
+					drive.stop();
+					nextStep(AutoSteps.FIRST_TURN);
 				}
+			} else {
+				if (drive.driveDistance(AUTO_SPEED, SWITCH_TICKS)) {
+					drive.stop();
+					nextStep(AutoSteps.LOADING);
+				}
+
+			}
+			break;
+		case FIRST_TURN:// add for both ways
+			if (switchPos.equals("R")) {
+				drive.turn(45, AUTO_SPEED);
+				} else {
+					drive.stop();
+					nextStep(AutoSteps.SECOND_STRAIGHT);
+				}
+				if (switchPos.equals("L")) {
+					drive.turn( -45, AUTO_SPEED);
+					} else {
+						drive.stop();
+						nextStep(AutoSteps.SECOND_STRAIGHT);
+					}
+				
+			
+			break;
+			case SECOND_STRAIGHT:
+			if (drive.driveDistance(AUTO_SPEED, HYPO_SWITCH)) {
+				drive.stop();
+				nextStep(AutoSteps.LOADING);
 			}
 			break;
 		case LOADING:
 			grabber.ungrab();
 			nextStep(AutoSteps.DONE);
 			break;
-		default:
+		
+				
 		case DONE:
 			nextStep(AutoSteps.DONE);
 			break;
+			
+		default:
+				nextStep(AutoSteps.DONE);
+				break;
 		}
 	}
 }
